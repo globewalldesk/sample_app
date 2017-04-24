894feab9acd6acdff3feb61be0e1f5268971fbc9
Larry Sanger <yo.larrysanger@gmail.com>
2017-04-24 19:11:02 +0000

    add password logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase } # downcases email before saving so case-sensitive indexers don't break
+  before_save { email.downcase! } # downcases email before saving so case-sensitive indexers don't break
   validates :name,  presence: true,  length: { maximum: 50 } # auto-validation, very cool
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i   # used to check that input email is hip to the groove
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i   # used to check that input email is hip to the groove
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }    # since emails must be unique, checks before saving
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+  validates :password, format: { with: /[a-z]+/ }
+  validates :password, format: { with: /[A-Z]+/ }
+  validates :password, format: { with: /\d+/ }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,20 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "Foobar1", password_confirmation: "Foobar1")
   end
 
   test "should be valid" do
     assert @user.valid?
+  end
+
+  
+  # name tests ##########################################
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    refute @user.valid?
   end
 
   test "name should be present" do
@@ -15,14 +24,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
+  
+  # email tests ##########################################
+  
   test "email should be present" do
     @user.email = "    "
     assert_not @user.valid?
-  end
-  
-  test "name should not be too long" do
-    @user.name = "a" * 51
-    refute @user.valid?
   end
   
   test "email should not be too long" do
@@ -41,7 +48,7 @@ class UserTest < ActiveSupport::TestCase
   
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -53,6 +60,31 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+  
+  test "emails should be downcased before saving" do
+    mixed_case_email = "IamUPPERandlower@gmail.COM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal @user.reload.email, mixed_case_email.downcase 
+  end
+
+  # password tests ##########################################
+  
+  test "passwords should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?, "password is too short"
+  end
+
+  test "passwords should include num and lc and uc letters" do
+    @user.password = @user.password_confirmation = "abcdef"
+    assert_not @user.valid?
+    @user.password = @user.password_confirmation = "Abcdef"
+    assert_not @user.valid?
+    @user.password = @user.password_confirmation = "abcdef5"
+    assert_not @user.valid?
+    @user.password = @user.password_confirmation = "Abcdef5"
+    assert @user.valid?
   end
 
 end
